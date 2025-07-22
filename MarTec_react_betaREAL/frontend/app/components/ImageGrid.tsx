f614a57ced542da83e9073eb70ad09f3be129e0b
Jeev Joel <72605659+Jeevjoel@users.noreply.github.com>
2025-07-22 20:36:20 +0530

    Update ImageGrid.tsx

--- MarTec_react_betaREAL/frontend/app/components/ImageGrid.tsx
+++ MarTec_react_betaREAL/frontend/app/components/ImageGrid.tsx
@@ -31,7 +31,7 @@ const collageImages = [
   { src: '/bubbles.png', alt: 'AI Generated Art 2', ratio: '1/1' },
   { src: '/bubbles2.png', alt: 'AI Generated Art 2', ratio: '1/1' },
   { src: '/cloth.png', alt: 'AI Generated Art 2', ratio: '1/1' },
-  { src: '/123.webp', alt: 'AI Generated Art 2', ratio: '1/1' },
+  { src: '/123.webp', alt: 'AI Generated Art 2', ratio: '512/512' },
   { src: '/florarose.png', alt: 'AI Generated Art 2', ratio: '1/1' },
   { src: '/lavender.png', alt: 'AI Generated Art 2', ratio: '1/1' },
   { src: '/mist.png', alt: 'AI Generated Art 2', ratio: '1/1' },
